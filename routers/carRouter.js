cc363660e4ec007d8d7fcd9aa2aa5e1e3cfb3ed1
unknown <neduvinci@gmail.com>
2019-11-05 15:35:45 +0100

    Completed MVP and 2 stretches

--- routers/carRouter.js
+++ routers/carRouter.js
@@ -25,6 +25,24 @@ router.get('/:id', validateCarId, (req, res) => {
     res.status(200).json(req.car)
 })
 
+router.delete('/:id', validateCarId, (req, res) => {
+    db('cars')
+        .where({ id: req.params.id })
+        .del()
+        .then(numberAffected => {
+            res.status(200).json({
+                message: numberAffected + " car has been removed."
+            })
+        })
+        .catch(error => {
+            res
+                .status(500)
+                .json({
+                message: "There was an error removing the car from the list" + " " + error
+            })
+        })
+})
+
 router.post('/', (req, res) => {
     db('cars').insert({
         vin: req.body.vin,
@@ -54,6 +72,39 @@ router.post('/', (req, res) => {
                 .status(500)
                 .json({
                 message: "There was an error adding the car to the list." + " " + error
+            })
+        })
+})
+
+router.put('/:id', (req, res) => {
+    db('cars').where({ id: req.params.id }).update({
+        vin: req.body.vin,
+        make: req.body.make,
+        model: req.body.model,
+        mileage: req.body.mileage,
+        transmissionType: req.body.transmissionType,
+        titleStatus: req.body.titleStatus
+    })
+        .then(numberUpdated => {
+            db('cars').where({ id: req.params.id })
+                .then(car => {
+                    res
+                        .status(200)
+                        .json({message: numberUpdated + " car has been updated", car: car[0]})
+                })
+                .catch(error => {
+                    res
+                        .status(500)
+                        .json({
+                        message: "There was an error getting the updated car from the database" + " " + error
+                    })
+                })
+        })
+        .catch(error => {
+            res
+                .status(500)
+                .json({
+                message: "There was an error updating the car." + " " + error
             })
         })
 })
